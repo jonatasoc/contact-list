c622ae1704851c645b91a797a091bd2781f63e60
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-05-13 22:37:06 -0300

    Add styles. Change img size

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -2,42 +2,53 @@ import styled from 'styled-components';
 
 export const Container = styled.main`
   display: flex;
-  flex-direction: column;
-  align-items: center;
 
   height: 100vh;
   width: 100%;
+  max-width: 900px;
   margin: 0 auto;
 
   background-color: var(--white);
 
-  padding: 0 50px;
-
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
   @media only screen and (max-width: 600px) {
     width: 100%;
   }
-`;
 
-export const Title = styled.p`
-  color: var(--blue);
+  > div {
+    height: 100vh;
+    width: 100%;
 
-  font-size: 30px;
-  letter-spacing: 1px;
-
-  margin-bottom: 40px;
+    max-width: 400px;
+  }
 `;
 
 export const HeroImg = styled.img`
   color: var(--blue);
-  width: 600px;
+  width: 50%;
+  max-width: 600px;
+  padding: 10px;
+
+  align-self: flex-start;
 
   font-size: 30px;
   letter-spacing: 1px;
 
   margin-bottom: 40px;
+  margin-right: -150px;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
+
+export const Title = styled.p`
+  color: var(--white);
+  background: var(--blue);
+  width: 100%;
+  height: 120px;
+  text-align: center;
+  padding-top: 40px;
+
+  font-size: 30px;
+  letter-spacing: 1px;
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
 
-import { Container, Title, HeroImg } from './Home.styles';
-import ContactUsForm from '../../components/ContactList';
+import { Container, HeroImg, Title } from './Home.styles';
+import Routes from '../../routes';
 
 import heroImg from '../../assets/images/personal_email.svg';
 
 const Home: React.FC = () => {
   return (
     <Box boxShadow={1}>
+      <Title>Contact List</Title>
       <Container>
-        <Title>Contact List</Title>
-        <main>
-          <ContactUsForm />
-          <HeroImg src={heroImg} />
-        </main>
+        <Routes />
+        <HeroImg src={heroImg} />
       </Container>
     </Box>
   );
