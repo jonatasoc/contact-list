9573d97de4488858abb464fb1f365a4b523661db
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-05-13 22:36:04 -0300

    Add routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
-import Home from './pages/Home';
+import ContactCardDetails from './components/ContactCardDetails';
+import ContactEditForm from './components/ContactEditForm';
+import ContactList from './components/ContactList';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/contact/edit/:id" component={ContactEditForm} />
+      <Route exact path="/contact/:id" component={ContactCardDetails} />
+      <Route exact path="/" component={ContactList} />
     </Switch>
   );
 };
