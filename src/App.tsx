cf1cf9f2e8569a5e0e3c9b6bbb959105f0d7bcc5
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-05-13 22:04:09 -0300

    Add Provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
-import Routes from './routes';
+import Home from './pages/Home';
+import { ContactsProvider } from './context/ContactsContext';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <ContactsProvider>
+      <Router>
+        <GlobalStyle />
+        <Home />
+      </Router>
+    </ContactsProvider>
   );
 };
 
