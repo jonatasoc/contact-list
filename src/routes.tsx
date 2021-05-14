731265eb4f5a6cad0fe51529905c922abbf57ca4
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-05-14 01:15:45 -0300

    Add new routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ContactCardDetails from './components/ContactCardDetails';
-import ContactEditForm from './components/ContactEditForm';
 import ContactList from './components/ContactList';
+import ContactAdd from './pages/ContactAdd';
+import ContactEdit from './pages/ContactEdit';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/contact/edit/:id" component={ContactEditForm} />
+      <Route exact path="/contact/edit/:id" component={ContactEdit} />
       <Route exact path="/contact/:id" component={ContactCardDetails} />
+      <Route exact path="/contact/add" component={ContactAdd} />
       <Route exact path="/" component={ContactList} />
     </Switch>
   );
